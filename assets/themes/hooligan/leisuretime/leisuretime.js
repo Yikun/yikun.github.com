38bbe2109873f18bb80012b5cc8027ef51ec527d
Yikun <yikunkero@gmail.com>
2014-04-20 23:30:01 +0800

    Update Bootstrap 3.1.1

--- assets/themes/hooligan/leisuretime/leisuretime.js
+++ assets/themes/hooligan/leisuretime/leisuretime.js
@@ -6,11 +6,11 @@ function addBookListall(leisuretimecontent){
 }
 function addBook(image, title, content, date){
 	var pic = document.createElement("DIV")
-	pic.setAttribute("class","span2");
+	pic.setAttribute("class","col-md-2");
 	pic.innerHTML="<img src=\""+ image +"\" width=\"128\" height=\"182\"><br>"
 	
 	var detail = document.createElement("DIV")
-	detail.setAttribute("class","span7");
+	detail.setAttribute("class","col-md-7");
 	detail.innerHTML="<h3><a>"+ title +"</a></h3><br><h4>"+date+"</h4><br>"+content
 	
 	var booklists = document.createElement("DIV")
